2ab793d26141aaadd67ae0f1f07b682b7561cd5d
Rhafael Chandra <rhafaelchandra@gmail.com>
2025-04-02 16:35:35 +0700

    feat: stored video will have prefix according to their aspect ratio

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -1,18 +1,29 @@
 package main
 
 import (
+	"bytes"
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"io"
+	"math"
 	"mime"
 	"net/http"
 	"os"
+	"os/exec"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
 )
+
+type VideoMetadata struct {
+	Streams []struct {
+		Width  int `json:"width"`
+		Height int `json:"height"`
+	} `json:"streams"`
+}
 
 func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {
 	r.Body = http.MaxBytesReader(w, r.Body, 10<<30)
@@ -82,7 +93,18 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	key := getAssetPath(mediaType)
+	aspectRatio, err := getVideoAspectRatio(tempFile.Name())
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't get video aspect ratio", err)
+		return
+	}
+	if aspectRatio == "16:9" {
+		aspectRatio = "landscape"
+	} else if aspectRatio == "9:16" {
+		aspectRatio = "portrait"
+	}
+
+	key := fmt.Sprintf("%s/%s", aspectRatio, getAssetPath(mediaType))
 	_, err = cfg.s3Client.PutObject(r.Context(), &s3.PutObjectInput{
 		Bucket:      aws.String(cfg.s3Bucket),
 		Key:         aws.String(key),
@@ -102,4 +124,51 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 		return
 	}
 	respondWithJSON(w, http.StatusOK, nil)
+}
+
+func getVideoAspectRatio(filepath string) (string, error) {
+	cmd := exec.Command(
+		"ffprobe",
+		"-v",
+		"error",
+		"-print_format",
+		"json",
+		"-show_streams",
+		filepath,
+	)
+
+	var out bytes.Buffer
+	cmd.Stdout = &out
+
+	if err := cmd.Run(); err != nil {
+		return "", err
+	}
+
+	metadata := &VideoMetadata{}
+	if err := json.Unmarshal(out.Bytes(), metadata); err != nil {
+		return "", err
+	}
+
+	width := metadata.Streams[0].Width
+	height := metadata.Streams[0].Height
+	tolerance := 0.01
+
+	aspectRatio := checkAspectRatioType(width, height, tolerance)
+
+	return aspectRatio, nil
+}
+
+func checkAspectRatioType(width, height int, tolerance float64) string {
+	sixteenNineRatio := 16.0 / 9.0
+	nineSixteenRatio := 9.0 / 16.0
+
+	ratio := float64(width) / float64(height)
+
+	if math.Abs(ratio-sixteenNineRatio) <= tolerance {
+		return "16:9"
+	}
+	if math.Abs(ratio-nineSixteenRatio) <= tolerance {
+		return "9:16"
+	}
+	return "other"
 }
